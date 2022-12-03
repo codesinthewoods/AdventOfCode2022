9bf09a15a6ff5647058eaec2e452a81a5c6f79c9
mpenner <mpenner@jensenhughes.com>
2022-12-02 19:55:42 -0500

    Add project files.

---
+++ Day1Project1/Program.cs
@@ -0,0 +1,126 @@
+﻿class Program
+{
+    static void CalculateMostCalories(string filename)
+    {
+        try
+        {
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = File.ReadAllLines(filename);
+
+            long mostCalories = 0;
+            long currentCalories = 0;
+            foreach (string line in lines)
+            {
+                // we have reached the end of a particular elf
+                if (line.Trim() == "")
+                {
+                    if (currentCalories > mostCalories)
+                    {
+                        mostCalories = currentCalories;
+                    }
+                    currentCalories = 0;
+                }
+                else
+                {
+                    long calories = long.Parse(line.Trim());
+
+                    currentCalories += calories;
+                }
+            }
+
+            Console.WriteLine("Most calories = " + mostCalories);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static void CalculateTopMostCalories(string filename, int numRanks)
+    {
+        try
+        {
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = File.ReadAllLines(filename);
+
+            long[] mostCalories = new long[numRanks];
+            // initialize array to 0
+            for (int i = 0; i < numRanks; i++)
+            {
+                mostCalories[i] = 0;
+            }
+
+            long currentCalories = 0;
+            foreach (string line in lines)
+            {
+                // we have reached the end of a particular elf
+                if (line.Trim() == "")
+                {
+                    UpdateRankings(mostCalories, currentCalories);
+                    currentCalories = 0;
+                }
+                else
+                {
+                    long calories = long.Parse(line.Trim());
+
+                    currentCalories += calories;
+                }
+            }
+
+            long totalCalories = 0;
+            Console.WriteLine("Most calories = " + mostCalories);
+            for (int i = 0; i < mostCalories.Length; i++)
+            {
+                Console.WriteLine(mostCalories[i]);
+                totalCalories += mostCalories[i];
+            }
+            Console.WriteLine("Total calories = " + totalCalories);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static void UpdateRankings(long[] mostCalories, long currentCalories)
+    {
+        for (int i = 0; i < mostCalories.Length; i++)
+        {
+            // find position and insert
+            if (currentCalories > mostCalories[i])
+            {
+                InsertInArray(mostCalories, currentCalories, i);
+                break;
+            }
+        }
+    }
+
+    static void InsertInArray(long[] longArray, long newItem, int insertAtIndex)
+    {
+        // loop from bottom up to position to insert
+        for (int i = longArray.Length - 1; i > insertAtIndex; i--)
+        {
+            longArray[i] = longArray[i - 1];
+        }
+        longArray[insertAtIndex] = newItem;
+    }
+
+    static void Main(string[] args)
+    {
+        string filename;
+        if (args.Length == 0)
+        {
+            filename = @"c:\temp\Day1Project1Input.txt";
+        }
+        else
+        {
+            filename = args[0];
+        }
+        CalculateMostCalories(filename);
+        CalculateTopMostCalories(filename, 3);
+        return;
+    }
+
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,166 @@
+﻿class Program
+{
+    static void CalculateTotalScoreByPlayerMoves(string filename)
+    {
+        try
+        {
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = File.ReadAllLines(filename);
+
+            int totalScore = 0;
+            foreach (string line in lines)
+            {
+                if (line.Trim() != "")
+                {
+                    string[] plays = line.Split(' ');
+                    if (plays.Length == 2)
+                    {
+                        totalScore += CalculateRoundScore(plays[0], plays[1]);
+                    }
+                }
+            }
+
+            Console.WriteLine("Total score = " + totalScore);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static int CalculateRoundScore(string opponentPlay, string playerPlay)
+    {
+        int roundScore = 0;
+
+        switch(playerPlay)
+        {
+            case "X": // Rock
+                roundScore += 1;
+                switch (opponentPlay)
+                {
+                    case "A": // Rock
+                        roundScore += 3;
+                        break;
+
+                    case "B": // Paper
+                        roundScore += 0;
+                        break;
+
+                    case "C": // Scissors
+                        roundScore += 6;
+                        break;
+                    default:
+                        throw new Exception("Invalid input");
+                }
+                break;
+
+            case "Y": // Paper
+                roundScore += 2;
+                switch (opponentPlay)
+                {
+                    case "A": // Rock
+                        roundScore += 6;
+                        break;
+
+                    case "B": // Paper
+                        roundScore += 3;
+                        break;
+
+                    case "C": // Scissors
+                        roundScore += 0;
+                        break;
+                    default:
+                        throw new Exception("Invalid input");
+                }
+                break;
+
+            case "Z": // Scissors
+                roundScore += 3;
+                switch (opponentPlay)
+                {
+                    case "A": // Rock
+                        roundScore += 0;
+                        break;
+
+                    case "B": // Paper
+                        roundScore += 6;
+                        break;
+
+                    case "C": // Scissors
+                        roundScore += 3;
+                        break;
+                    default:
+                        throw new Exception("Invalid input");
+                }
+
+                break;
+            default:
+                throw new Exception("Invalid input");
+        }
+
+        return roundScore;
+    }
+
+    static void CalculateTotalScoreByStrategy(string filename)
+    {
+        try
+        {
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = File.ReadAllLines(filename);
+
+            int totalScore = 0;
+            foreach (string line in lines)
+            {
+                if (line.Trim() != "")
+                {
+                    totalScore += CalculateRoundScoreByStrategy(line);
+                }
+            }
+
+            Console.WriteLine("Total score by strategy = " + totalScore);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    static int CalculateRoundScoreByStrategy(string strategy)
+    {
+        Dictionary<string, int> strategyToScore =
+              new Dictionary<string, int>()
+              {
+                {"A X", 3 + 0}, // Rock vs Scissors
+                {"A Y", 1 + 3}, // Rock vs Rock
+                {"A Z", 2 + 6}, // Rock vs Paper
+                {"B X", 1 + 0}, // Paper vs Rock
+                {"B Y", 2 + 3}, // Paper vs Paper
+                {"B Z", 3 + 6}, // Paper vs Scissors
+                {"C X", 2 + 0}, // Scissors vs Paper
+                {"C Y", 3 + 3}, // Scissors vs Scissors
+                {"C Z", 1 + 6} // Scissors vs Rock
+              };
+        int roundScore = strategyToScore[strategy];
+
+        return roundScore;
+    }
+
+    static void Main(string[] args)
+    {
+        string filename;
+        if (args.Length == 0)
+        {
+            filename = @"c:\temp\Day2Input.txt";
+        }
+        else
+        {
+            filename = args[0];
+        }
+        CalculateTotalScoreByPlayerMoves(filename);
+        CalculateTotalScoreByStrategy(filename);
+        return;
+    }
+
+}
